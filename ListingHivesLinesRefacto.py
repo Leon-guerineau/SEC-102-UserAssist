7d97e22dbc8eb785d5e556d74620fc99b35916dd
Léon Guérineau <guerineauleon@gmail.com>
2023-02-16 16:53:20 +0100

    16-02-2023

--- ListingHivesLinesRefacto.py
+++ ListingHivesLinesRefacto.py
@@ -1,4 +1,6 @@
 import subprocess
+import codecs
+from struct import unpack
 
 """
 Fonction qui liste les sous clés de la clé de registre UserAssist
@@ -17,7 +19,7 @@ def getHivesPathList() :
         
         # Conversion d'UTF-8 à une chaine de caractères banale
         try :
-            decodedHivePath = hivePath.decode('UTF-8')
+            decodedHivePath = hivePath.decode('cp1252')
             if(decodedHivePath != '') :
                 hivesPathList.append(decodedHivePath)
         except UnicodeError :
@@ -26,8 +28,11 @@ def getHivesPathList() :
     # On retourne les sous clés
     return hivesPathList
 
+
+
 """
 Fonction qui liste les lignes d'une sous clé donnée
+    - hivePath : le chemin de la sous clé
 """
 def getHiveLines(hivePath) :
     # Commande pour lister les lignes du répertoire \count de la sous clé 
@@ -42,25 +47,127 @@ def getHiveLines(hivePath) :
     # On parcours, décode et renvois les résultats de la commande
     hiveLines = []
     for line in linesQuery :
-
-        # Conversion d'UTF-8 à une chaine de caractères banale
+        
+        # Conversion de cp1252 à une chaine de caractères banale
         try :
-            decodedLine = line.decode('UTF-8')
+            decodedLine = line.decode('cp1252')
             if(decodedLine != '') :
                 hiveLines.append(decodedLine)
         except UnicodeError :
-            print('+1 ligne en erreur de décodage')
-    
+            print('+1 ligne en erreur de décodage : ', line)
+            
     # On retourne les lignes
     return hiveLines
-       
+
+
+
+"""
+Fonction qui récupère l'uuid d'une chaine de caractères
+    - inputStr : la chaine de caractères
+"""
+def getUuid(inputStr) :
+    isUuid = False
+    result = ''
+    for char in inputStr :
+        
+        # Si le caractère est { alors ce caratère et les caratères suivant font partie de l'uuid
+        if (char == '{') :
+            isUuid = True
+            
+        # Si le caractère fait partie de l'uuid alors on le stocke
+        if(isUuid) :
+            result += char
+            
+        # Si le caractère est } alors les caratères suivant ne font pas partie de l'uuid
+        if (char == '}') :
+            isUuid = False
+            
+    # On retourne l'uuid ou une chaine vide
+    return result
+
+
+
+"""
+Fonction qui retire l'uuid contenu dans une chaine de caractères s'il y en a un
+    - inputStr : la chaine de caractères
+"""
+def removeUuid(inputStr) :
+    uuid = getUuid(inputStr)
+    
+    # S'il y a un uuid alors on le retire et on retourne la chaine
+    if(uuid != '') :
+        return inputStr.replace(uuid, "")
+    
+    # Sinon on retourne la chaine non changée
+    else :
+        return inputStr
+
+
+"""
+Fonction qui retourne le chemin d'un logiciel pour une ligne donnée
+    - line : la ligne au format '    (chemin)    (type)    (données)'
+    - decoded : booléen déterminant si le résultat est décodeé en rot 13 (vrai par défaut)
+"""
+def getProgramPath(line, decoded = True) :
+    splittedLine = line.split('    ')
+    
+    # On retire le premier élément qui est créé par la tabulation en début de ligne
+    if(splittedLine[0] == '') :
+        splittedLine.pop(0)
+    programPath = removeUuid(splittedLine[0])
+    
+    if(decoded) :
+        # On retourne le chemin déchiffré en rot13
+        return codecs.encode(programPath, 'rot13')
+    else :
+        return programPath
+    
+    
+    
+def getData(line, decoded = True) :
+    splittedLine = line.split('    ')
+    
+    # On retire le premier élément qui est créé par la tabulation en début de ligne
+    if(splittedLine[0] == '') :
+        splittedLine.pop(0)
+    data = removeUuid(splittedLine[2])
+    byteData = bytes.fromhex(data)
+    print(unpack("I", byteData[4:8])[0], unpack("I", byteData[12:16])[0], unpack("Q", byteData[60:68])[0])
+
 """
 Programme Pricipal
 """
+encodedData = []
+decodedData = []
+
+# On récupère et parcourt les liens des sous clés
 hivesPathList = getHivesPathList()
 for hivePath in hivesPathList :
+    
+    # On récupère et parcourt les lignes de la sous clé si elle en a
     hiveLines = getHiveLines(hivePath)
     if(len(hiveLines) != 0) :
-        print(hiveLines)
+        
+        # Ajout du chemin de la sous clé
+        encodedData.append(hivePath)
+        decodedData.append(hivePath)
+        
+        # Ajout des liens de programmes
+        for line in hiveLines :
+            getData(line)
+            encodedData.append('	'+getProgramPath(line, False))
+            decodedData.append('	'+getProgramPath(line))
+        
+        # Ajout d'une ligne pour faire un saut de ligne
+        encodedData.append('')
+        decodedData.append('')
+
+# Écriture des données encodés dans le fichier userassist.txt
+with open("userassist.txt", "wb") as file:
+    file.write("\n".join(encodedData).encode('utf-8'))
+
+# Écriture des données décodés dans le fichier decode_userassist.txt
+with open("decode_userassist.txt", "wb") as file:
+    file.write("\n".join(decodedData).encode('utf-8'))
 
                 
